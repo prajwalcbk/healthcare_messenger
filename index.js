3dc55693e5b7e8ca9336f5d97c58dff581748184
prajwalcbk <65850053+prajwalcbk@users.noreply.github.com>
2024-04-13 14:31:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,15 +8,24 @@ app.use(cors());
 app.use(express.json());
 
 
+
+
 const server = app.listen(process.env.PORT, () =>
   console.log(`Server started on ${process.env.PORT}`)
 );
 const io = socket(server, {
   cors: {
-    origin: "http://192.168.1.2:3000",
+    origin: "https://sxl9752.uta.cloud/",
     credentials: true,
   },
 });
+
+
+app.get('/health', (req, res) => {
+  res.status(200).json({ status: 'UP' });
+});
+
+
 
 global.onlineUsers = new Map();
 io.on("connection", (socket) => {
